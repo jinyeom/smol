aaf8f66cc14f1e8ca19373e4347c4c625d0cbaac
jinyeom <jinyeom95@gmail.com>
2020-07-21 17:09:01 -0500

    minor changes

--- smol/utils/darknet.py
+++ smol/utils/darknet.py
@@ -20,23 +20,23 @@ def darknet_numel(model: nn.Module) -> int:
 def _load_conv_norm(weights: np.ndarray, module: ConvLayer) -> np.ndarray:
     norm_bias_numel = module.norm.bias.numel()
     norm_bias, weights = weights[:norm_bias_numel], weights[norm_bias_numel:]
-    norm_bias = torch.from_numpy(norm_bias).reshape(module.norm.bias.data.shape)
+    norm_bias = torch.from_numpy(norm_bias).view_as(module.norm.bias)
 
     norm_weight_numel = module.norm.weight.numel()
     norm_weight, weights = weights[:norm_weight_numel], weights[norm_weight_numel:]
-    norm_weight = torch.from_numpy(norm_weight).reshape(module.norm.weight.data.shape)
+    norm_weight = torch.from_numpy(norm_weight).view_as(module.norm.weight)
 
     norm_mean_numel = module.norm.running_mean.numel()
     norm_mean, weights = weights[:norm_mean_numel], weights[norm_mean_numel:]
-    norm_mean = torch.from_numpy(norm_mean).reshape(module.norm.running_mean.data.shape)
+    norm_mean = torch.from_numpy(norm_mean).view_as(module.norm.running_mean)
 
     norm_var_numel = module.norm.running_var.numel()
     norm_var, weights = weights[:norm_var_numel], weights[norm_var_numel:]
-    norm_var = torch.from_numpy(norm_var).reshape(module.norm.running_var.data.shape)
+    norm_var = torch.from_numpy(norm_var).view_as(module.norm.running_var)
 
     conv_weight_numel = module.conv.weight.numel()
     conv_weight, weights = weights[:conv_weight_numel], weights[conv_weight_numel:]
-    conv_weight = torch.from_numpy(conv_weight).reshape(module.conv.weight.data.shape)
+    conv_weight = torch.from_numpy(conv_weight).view_as(module.conv.weight)
 
     module.norm.bias.data.copy_(norm_bias)
     module.norm.weight.data.copy_(norm_weight)
@@ -50,11 +50,11 @@ def _load_conv_norm(weights: np.ndarray, module: ConvLayer) -> np.ndarray:
 def _load_conv(weights: np.ndarray, module: ConvLayer) -> np.ndarray:
     conv_bias_numel = module.conv.bias.numel()
     conv_bias, weights = weights[:conv_bias_numel], weights[conv_bias_numel:]
-    conv_bias = torch.from_numpy(conv_bias).reshape(module.conv.bias.data.shape)
+    conv_bias = torch.from_numpy(conv_bias).view_as(module.conv.bias)
 
     conv_weight_numel = module.conv.weight.numel()
     conv_weight, weights = weights[:conv_weight_numel], weights[conv_weight_numel:]
-    conv_weight = torch.from_numpy(conv_weight).reshape(module.conv.weight.data.shape)
+    conv_weight = torch.from_numpy(conv_weight).view_as(module.conv.weight)
 
     module.conv.bias.data.copy_(conv_bias)
     module.conv.weight.data.copy_(conv_weight)
@@ -67,6 +67,11 @@ def load_darknet_weights(model: nn.Module, weights_path: Union[str, Path]) -> bo
     with open(weights_path, "rb") as f:
         header = np.fromfile(f, count=5, dtype=np.int32)
         weights = np.fromfile(f, dtype=np.float32)
+
+    model_name = Path(weights_path).stem
+    major, minor, revision, seen, _ = header
+    version = f"v{major}.{minor}.{revision}-{seen}"
+    print(f"Loading {model_name} {version}...")
 
     if darknet_numel(model) != len(weights):
         raise ValueError("Unmatching number of weights")
